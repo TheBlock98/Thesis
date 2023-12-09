91af81a339dacd8afd66432747b5ee51fd388c1a
The Block <88546749+TheBlock98@users.noreply.github.com>
2023-12-09 10:40:41 +0100

    DataPreProcessingpt1
    Stationarity check

---
+++ FeatureEngineering&DATA/DataPreProcessing.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Stationarity check 
+
+# We control the stationarity of dataSet in order to be sure that ML theory is applicabile in simple way otherwise we have to use online machine learning tecnique 
+
+# In[3]:
+
+
+import plotly.io as pio
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+import pandas as pd
+
+pio.renderers.default = 'notebook'
+dataSet= pd.read_csv("/Users/stefanobutera/Desktop/DocTesi/NewTesi/Code/data/DataSet/dataSetIMRCleaned.csv ")
+
+
+fig, axs = plt.subplots(nrows=11, ncols=6, figsize=(20, 30))
+fig.suptitle('Distribution probability of features', y=1.02)
+
+
+
+for i, col in enumerate(dataSet.iloc[:, 1:]):
+  sns.histplot(dataSet[col], ax=axs[i // 6, i % 6])
+  axs[i // 6, i % 6].set_title(col)
+
+plt.tight_layout()
+plt.show()
+
+
+
+
+# In[7]:
+
+
+fig, axs = plt.subplots(nrows=11, ncols=6, figsize=(20, 30))
+fig.suptitle('Distribution probability of features', y=1.02)
+
+
+
+for i, col in enumerate(dataSet.iloc[:, 1:]):
+    sns.lineplot(x=dataSet.index, y=dataSet[col], ax=axs[i // 6, i % 6])
+    axs[i // 6, i % 6].set_title(col)
+
+plt.tight_layout()
+plt.show()
+
+
+# In[8]:
+
+
+# probability distribution of target
+plt.figure(figsize=(8, 6))
+sns.histplot(dataSet['hlc3'], kde=True, color='skyblue')
+plt.title('Distribuzione di hlc3')
+plt.xlabel('hlc3')
+plt.ylabel('Frequenza')
+plt.show()
+
+
+# now we comput the AdFuller test
+
+# In[25]:
+
+
+from statsmodels.tsa.stattools import adfuller
+import colored
+import numpy as np
+
+
+dataSet.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+
+dataSet.fillna(dataSet.mean(), inplace=True)
+
+
+threshold = 0.05
+
+for col in dataSet.iloc[:, 1:]:
+    result = adfuller(dataSet[col])
+    print(f'Colonna: {col}')
+    print('ADF Statistic:', result[0])
+    print('p-value:', result[1])
+
+    # threshold checker 
+    if result[1] <= threshold:
+        print(colored.stylize('Stazionario (p-value <= {:.2f})'.format(threshold), colored.fg('green')))
+    else:
+        print(colored.stylize('Non stazionario (p-value > {:.2f})'.format(threshold), colored.fg('red')))
+    
+    print('-' * 40)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
