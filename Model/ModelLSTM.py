2de091d46b6c9de086db18e3941c7c4d8e99e5e0
The Block <88546749+TheBlock98@users.noreply.github.com>
2023-12-20 18:41:11 +0100

    Update ModelLSTM.py
    aggiustamenti piccoli e variegati

--- Model/ModelLSTM.py
+++ Model/ModelLSTM.py
@@ -1,8 +1,9 @@
+
 """
 We want to build a LSTM model to predict future price  
 (HLC3) of BTC using dataSetIMRCleaned.csv like dataSet.
 First of all  we want to predict HLC3 to one step, in the second phase we want build multiOutput model to
-predict HLC3 to:  1 steps, 24 steps, 48 steps, 168 steps. We implement using pyTorch.
+predict HLC3 to:  1 steps in this phase. We implement using pyTorch.
 
 We want use Boruta to do PCA and select important features.
 """
@@ -29,7 +30,7 @@ config = {
     "model": {
         "input_size": 62,  # since we are only using 62 feature, close price
         "num_lstm_layers": 2,
-        "lstm_size": 128,
+        "lstm_size": 64,
         "dropout": 0.8,
         "num_class": 1,
         "model_path": "Model/modelLSTMV0.pth",
@@ -37,7 +38,7 @@ config = {
         "save_model": True,
     },
     "training": {
-        "device": "cpu",  # "cuda" or "cpu"
+        #"device": "cpu",  # "cuda" or "cpu"
         "batch_size": 64,
         "num_epochs": 100,
         "learning_rate": 0.01, # 1e-3
@@ -54,27 +55,18 @@ config = {
         
       },
 }
-      
-    
-""" 
-device = (
-  "cuda"
-  if torch.cuda.is_available()
-  else "mps"
-  if torch.backends.mps.is_available()
-  else "cpu"
-)
-print(f"Using {device} device")
 
-"""
-
-dataSet = pd.read_csv("Model/dataSetIMRNormalized.csv")
+dataSet = pd.read_csv("/content/dataSetIMRNormalized.csv")
 hlc3 = dataSet['hlc3']
 dataSet.drop(labels=['hlc3'], axis=1, inplace=True)
 dataSet.insert(0, 'hlc3', hlc3)
 
 # Dataloading (sliding windows)
 config["model"]["input_size"] = dataSet.shape[1] # with 1 have number of features
+
+
+from google.colab import drive
+drive.mount('/content/drive')
 
 def sliding_windows(df, windowSize):
   x = []
@@ -99,27 +91,28 @@ test_size = int(len(y) * config["data"]["test_split_size"])
 dataX = Variable(torch.Tensor(np.array(x)))
 dataY = Variable(torch.Tensor(np.array(y)))
 
+print(dataX.size())
 # split seqences into train and test
 trainX = Variable(torch.Tensor(np.array(x[0:train_size])))
 trainY = Variable(torch.Tensor(np.array(y[0:train_size])))
 valX = Variable(torch.Tensor(np.array(x[train_size:train_size + val_size])))
 valY = Variable(torch.Tensor(np.array(y[train_size:train_size + val_size])))
-
+print(valX.size())
 testX = Variable(torch.Tensor(np.array(x[train_size + val_size:])))
 testY = Variable(torch.Tensor(np.array(y[train_size + val_size:])))
 
-# define LSTM V0 model
+
 class LSTM(nn.Module):
   def __init__(self, input_size, num_lstm_layers, lstm_size, dropout, num_class):
     super(LSTM, self).__init__()
-    self.input_size = input_size
+    self.input_size = config["model"]["input_size"]
     self.num_lstm_layers = num_lstm_layers
     self.lstm_size = lstm_size
     self.dropout = dropout
     self.num_class = num_class
 
     # Fully connected layer as input layer
-    self.fc_input = nn.Linear(input_size, lstm_size) 
+    self.fc_input = nn.Linear(62, lstm_size) 
 
     
     # Define the LSTM layers
@@ -140,7 +133,7 @@ class LSTM(nn.Module):
   def forward(self, x):
 
     # Pass data through the input fully connected layer
-    x = F.relu(self.fc_input(x))
+    x = F.relu(x)
     
     # Initialize hidden and cell states with zeros
     h_0 = Variable(torch.zeros(self.num_lstm_layers, x.size(0), self.lstm_size))
@@ -158,12 +151,11 @@ class LSTM(nn.Module):
     out = self.fc(out)
     return out
 
-
 model = LSTM(input_size=config["model"]["input_size"],
              num_lstm_layers=config["model"]["num_lstm_layers"],
              lstm_size=config["model"]["lstm_size"],
              dropout=config["model"]["dropout"],
-             num_class=y.shape[1])
+             num_class=1)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=config["training"]["learning_rate"])
 # optimizer = optim.RMSprop(model.parameters(), lr=config["training"]["learning_rate"], weight_decay=weight_decay)
@@ -174,12 +166,12 @@ scheduler = config["training"]["scheduler"](optimizer, patience=config["training
                                             factor=config["training"]["scheduler_factor"], 
                                             min_lr=config["training"]["scheduler_min_lr"])
 
-# training loop:
-
-# training loop:
 
 best_val_loss = float('inf')
 patience_counter = 0
+
+class EarlyStoppingException(Exception):
+  pass
 
 def training(model, optimizer, criterion, scheduler, trainX, trainY, valX, valY, epoch, best_val_loss,patience_counter):
   print(f"Epoch {epoch+1} \n ---------- ")
@@ -209,16 +201,19 @@ def training(model, optimizer, criterion, scheduler, trainX, trainY, valX, valY,
           if patience_counter >= config["training"]["early_stopping_patience"]:
               print(f'Early stopping: val_loss has not improved ' +
                     f'for {config["training"]["early_stopping_patience"]} consecutive epochs.')
-              break  # Break out of the training loop
+              raise EarlyStoppingException()
+              
+    
   # Output training/validation statistics
   print(f'Epoch[{epoch+1}/{config["training"]["num_epochs"]}] | ' +
         f'Loss train: {loss.item():.6f}, ' +
         f'Loss val: {val_loss.item():.6f} | ' +
-        f'LR: {optimizer.param_groups[0]["lr"]:.6f}')
+        f'LR: {optimizer.param_groups[0]["lr"]:.6f}'+
+       f"best validation loss: {best_val_loss:.6f}" )
   return loss, val_loss, best_val_loss, patience_counter
 
-  
-  def testing(model,testX,testY,criterion):
+
+def testing(model,testX,testY,criterion):
     # Test the model
     model.eval()
     test_loss, correct = 0, 0
@@ -238,23 +233,22 @@ def training(model, optimizer, criterion, scheduler, trainX, trainY, valX, valY,
           f'Accuracy: {kpy_accuracy:.6f}, R2: {kpy_r2:.6f}')
     return test_outputs,kpy_mse, kpy_rmse, kpy_mae, kpy_r2,kpy_accuracy
 
-    
 
-  for epoch in range(config["training"]["num_epochs"]):
-    print(f"Epoch {t+1}\n-------------------------------")
-    loss, val_loss, best_val_loss, patience_counter = training(model, optimizer, criterion, scheduler, trainX, trainY, valX, valY, epoch,best_val_loss,patience_counter)
-    test_outputs,kpy_mse, kpy_rmse, kpy_mae, kpy_r2,kpy_accuracy = testing(model,testX,testY,criterion))
-  print("Done!")
-
-
-
+for epoch in range(config["training"]["num_epochs"]):
+    try:
+        print(f"Epoch {epoch + 1}\n-------------------------------")
+        loss, val_loss, best_val_loss, patience_counter = training(model, optimizer, criterion, scheduler, trainX, trainY, valX, valY, epoch, best_val_loss, patience_counter)
+        test_outputs, kpy_mse, kpy_rmse, kpy_mae, kpy_r2, kpy_accuracy = testing(model, testX, testY, criterion)
+    except EarlyStoppingException:
+        print("Early stopping triggered, proceeding to the next epoch...")
+        break
+    print(f"Best validation loss: {best_val_loss:.6f}")
+print("Done!")
 
 
 # Save the model
 torch.save(model.state_dict(), config["training"]["model_path"])
 print(f'ModelLSTMV0 saved to {config["training"]["model_path"]}')
-
-
 
 
 data_predict = test_outputs.data.numpy()
@@ -275,9 +269,6 @@ plt.legend()
 plt.axvline(x=train_size, c='r', linestyle='--')
 plt.suptitle('Time-Series Prediction')
 plt.show()
-
-
-
 
 
 
